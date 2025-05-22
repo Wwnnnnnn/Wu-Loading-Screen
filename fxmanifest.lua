045399d3e02c625596df9b96ec2dcd74d7b82f6f
! Maître Wu <nayroxwwn@gmail.com>
2025-05-22 18:17:29 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Wu'
+description 'LoadingScreen Wu" (https://discord.gg/8k5wqBWYg7)
+loadscreen 'index.html'
+
+version '2.0.0'  
+shield_script_protection 'enable'
+shield_script_cache 'disable'
+shield_script_loader 'refresh'
+adhesive_cdn_cache 'disable'
+
+files {
+    'index.html',
+    'music/track1.mp3',
+    'music/track2.mp3',
+    'music/track3.mp3',
+    'music/track4.mp3',
+    'music/track5.mp3',
+    'music/track6.mp3',
+    'music/track7.mp3',
+    'music/track8.mp3',
+    'music/track9.mp3',
+    'music/track10.mp3',
+    'music/track11.mp3',
+    'music/track12.mp3',
+    'music/track13.mp3',
+    'discord-logo.png',
+    'background.png'
+}
